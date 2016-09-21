e1f6a623f8d56647b149f1c6d9dc88dbf8d4dd78
Denis Duan <denisduan@gmail.com>
2016-09-22 00:07:09 +1000

    Add room name to the chat app

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,17 @@
  var socket = io();
  var name = getQueryVariable('name') || 'Anonymous';
- var room = getQueryVariable('room');
+ var room = getQueryVariable('room') || 'Room';
 
+ // Set up the room name
  console.log(name + ' wants to join the ' + room);
+ $('h1.room-title').text(room);
 
+ // Trigger the 'connet' event that informs the server that user connected
  socket.on('connect', function() {
      console.log('Connected to socket.io server!');
  });
 
+ // Listens to the 'message' event from server 
  socket.on('message', function(message) {
 
      // Get the timestamp in utc
@@ -17,13 +21,15 @@
      console.group('New message:');
      console.log(message.text);
 
-     messageElemObj.append('<p><strong>' + message.name + ' '+ momentTimestamp.local().format('h:mm:ss a') + ': </strong></p>');
+    // Adds the new incoming message to the page
+     messageElemObj.append('<p><strong>' + message.name + ' ' + 
+        momentTimestamp.local().format('h:mm:ss a') + ': </strong></p>');
      messageElemObj.append('<p>' + message.text + '</p>');
  })
 
  //Handles submitting new message
  var form = $('#message-form');
- form.on('submit', function (event) {
+ form.on('submit', function(event) {
      event.preventDefault();
      var inputElemObj = form.find('input[name=message]');
      socket.emit('message', {
@@ -32,6 +38,5 @@
      });
 
      // Clean up the text field after commit
-     inputElemObj.val(''); 
-
+     inputElemObj.val('');
  });
